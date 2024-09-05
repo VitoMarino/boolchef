50e2863e5f896b05c17b7eacf04ccf226239e7a7
CristianN01 <cristiannechifor71@gmail.com>
2024-09-05 12:59:42 +0200

    aggiunto [

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(
+        $this->call([
             VoteSeeder::class,
             SponsorshipSeeder::class,
             SpecializationSeeder::class,
