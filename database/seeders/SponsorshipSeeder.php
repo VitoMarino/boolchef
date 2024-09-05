2930e955307f671d5b8da10fe659284d60a0453b
LorezoParis1996 <lorenzoparis86@live.it>
2024-09-05 13:34:05 +0200

    fixato seeder sponsor

--- database/seeders/SponsorshipSeeder.php
+++ database/seeders/SponsorshipSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Sponsorship;
+
 
 
 class SponsorshipSeeder extends Seeder
