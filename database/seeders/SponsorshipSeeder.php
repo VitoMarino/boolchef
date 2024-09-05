b898aec26f17d038cc2ec4f0dce3f7dabfeccf4e
MatteoDiMario96 <dm.matteo96@live.it>
2024-09-05 13:33:51 +0200

    fixato.

--- database/seeders/SponsorshipSeeder.php
+++ database/seeders/SponsorshipSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Sponsorship;
 
 
 class SponsorshipSeeder extends Seeder
