188d0ef85a377944ca4b042880478505d3c6bc58
MatteoDiMario96 <dm.matteo96@live.it>
2024-09-09 13:59:00 +0200

    Messe tutte le old nella create.

--- resources/views/admin/chefs/create.blade.php
+++ resources/views/admin/chefs/create.blade.php
@@ -2,39 +2,48 @@
 
 @section('content')
     <div class="container">
+        <!---https://www.youtube.com/watch?v=dQw4w9WgXcQ--->
         <!--Inzio del FORM-->
-        <form class="d-flex justify-content-center flex-column" action="{{ route('admin.chefs.store') }}" method="POST"
-            enctype="multipart/form-data">
-
+        <form class="d-flex justify-content-center flex-column" action="{{ route('admin.chefs.store') }}"
+            method="POST" enctype="multipart/form-data">
+            @method('POST')
             @csrf
             <label for="address">
                 <strong>
                     Indirizzo
                 </strong>
             </label>
-            <input type="text" name="address" class="form-control mb-3" placeholder='Address'>
+            <input type="text" value="{{ old('address') }}
+        " name="address"
+                class="form-control mb-3">
 
             <label for="telephone">
                 <strong>
                     Numero di telefono
                 </strong>
             </label>
-            <input type="text" name="telephone" class="form-control mb-3" placeholder="Telephone">
+            <input type="text" value="{{ old('telephone') }}
+        " name="telephone"
+                class="form-control mb-3">
 
             <label for="descriprion_of_dishes">
                 <strong>
                     Descrizione dei piatti
                 </strong>
             </label>
+            <textarea class="form-control mb-3" placeholder="Description of dishes" value="" name="description_of_dishes"
+                rows="5" cols="33">
+            {{ old('description_of_dishes') }}
 
-            <!--TEXT AREA-->
-            <textarea class="form-control mb-3" placeholder="Descriprion of dishes" name="descriprion_of_dishes"></textarea>
+        </textarea>
 
             <!--Input file--->
             <label for="photograph">Carica foto profilo</label>
             <input class="mb-3" type="file" name="photograph" id="photograph">
             <label for="file">Carica CV</label>
             <input class="mb-3" type="file" name="CV" id="CV">
+
+
 
             <!--Select della visibilità-->
             <label for="visibility">
@@ -43,10 +52,10 @@
                 </strong>
             </label>
             <select class="form-select mb-3" aria-label="Default select example" name="visibility" id="visibility">
-                <option value="0">
+                <option value="0"{{ old('visibility') == 0 ? 'selected' : '' }}>
                     Not visible
                 </option>
-                <option value="1">
+                <option value="1"{{ old('visibility') == 1 ? 'selected' : '' }}>
                     Visible
                 </option>
             </select>
@@ -59,14 +68,16 @@
             </label>
             <div class="d-flex flex-wrap" role="group" aria-label="Basic checkbox toggle button group">
                 @foreach ($specializations as $specialization)
-                    <input type="checkbox" class="btn-check" name="specializations[]" value="{{ $specialization->id }}"
-                        id="specialization-check-{{ $specialization->id }}" autocomplete="off">
-                    <label class="btn btn-outline-primary m-1" for="specialization-check-{{ $specialization->id }}">
+                    <input type="checkbox" name="specializations[]" value="{{ $specialization->id }}" class="btn-check"
+                        id="specialization-check-{{ $specialization->id }}" autocomplete="off"
+                        {{ in_array($specialization->id, old('specializations', [])) ? 'checked' : '' }}>
+                    <label class="btn btn-outline-primary m-1"
+                        for="specialization-check-{{ $specialization->id }}">
                         {{ $specialization->name }}
                     </label>
                 @endforeach
             </div>
-            <!---https://www.youtube.com/watch?v=dQw4w9WgXcQ--->
+
             <!--Button submit-->
             <div class="mt-4">
                 <input type="submit" class="btn btn-info">
