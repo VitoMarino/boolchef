3c7fbc52b770eddcc974ac9e7fc24c2158340b04
LorezoParis1996 <lorenzoparis86@live.it>
2024-09-09 12:02:52 +0200

    create request e popolate

--- app/Http/Controllers/Admin/ChefController.php
+++ app/Http/Controllers/Admin/ChefController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreChefRequest;
+use App\Http\Requests\UpdateChefRequest;
 use App\Models\Chef;
 use App\Models\Specialization;
 use App\Models\User;
@@ -37,15 +39,15 @@ class ChefController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreChefRequest $request)
     {
-        $data = $request->all();
+        $data = $request->validated();
 
 
-        // $img_path = Storage::disk('public')->put('uploads/Images', $data['photograph']);
-        //$file_path = Storage::disk('public')->put('uploads/Cv', $data['CV']);
-        // $data["photograph"] = $img_path;
-        // $data["CV"] = $file_path;
+        $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
+        $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
+        $data["photograph"] = $img_path;
+        $data["CV"] = $file_path;
         $data['user_id'] = Auth::id();
         $newChef = Chef::create($data);
         return redirect()->route('admin.chefs.show', $newChef);
@@ -73,9 +75,9 @@ class ChefController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateChefRequest $request, Chef $chef)
     {
-        $data = $request->validated([]);
+        $data = $request->validated();
         $img_path = Storage::disk('public')->put('uploads/Images', $data['photograph']);
         $file_path = Storage::disk('public')->put('uploads/Cv', $data['CV']);
         $data["photograph"] = $img_path;
---
+++ app/Http/Requests/StoreChefRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreChefRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "address"=>'nullable|min:3|max:150',
+            "CV"=>'nullable|min:10|max:250',
+            "photograph"=>'nullable|min:10|max:2000',
+            "telephone"=>'required|unique:chefs|min:7|max:15',
+            "specializations"=>'required|array|exists:specializations,id',
+        ];
+    }
+}
