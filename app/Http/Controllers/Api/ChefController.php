c430a095fd4dee9b905a854eca8c616f4f3ae0bf
SimoneColombo1 <simcolombo2@gmail.com>
2024-09-18 17:38:06 +0200

    Aggiunto ordine per sponsorships

--- app/Http/Controllers/Api/ChefController.php
+++ app/Http/Controllers/Api/ChefController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateChefRequest;
 use App\Models\Chef;
 use App\Models\Review;
 use App\Models\Specialization;
+use App\Models\Sponsorship;
 use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Http\Request;
@@ -67,30 +68,30 @@ class ChefController extends Controller
     //             "results" => $newChef
     //         ]);
     // }
-//     public function update(UpdateChefRequest $request, Chef $chef){
-//         $data = $request->validated();
+    //     public function update(UpdateChefRequest $request, Chef $chef){
+    //         $data = $request->validated();
 
-//         // Se nella request hai il file 'photograph' manda avanti la modifica. Altrimenti non fare nulla.
-//         if($request->hasFile('photograph')){
-//             if ($chef->photograph) {
-//                 Storage::disk('public')->delete($chef->photograph);
-//             }
-//             $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
-//             $data["photograph"] = $img_path;
-//         }
+    //         // Se nella request hai il file 'photograph' manda avanti la modifica. Altrimenti non fare nulla.
+    //         if($request->hasFile('photograph')){
+    //             if ($chef->photograph) {
+    //                 Storage::disk('public')->delete($chef->photograph);
+    //             }
+    //             $img_path = Storage::disk('public')->put('upload/img', $data['photograph']);
+    //             $data["photograph"] = $img_path;
+    //         }
 
-//         if($request->hasFile('CV')){
-//             if ($chef->CV) {
-//                 Storage::disk('public')->delete($chef->CV);
-//             }
-//             $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
-//             $data["CV"] = $file_path;
-//         }
+    //         if($request->hasFile('CV')){
+    //             if ($chef->CV) {
+    //                 Storage::disk('public')->delete($chef->CV);
+    //             }
+    //             $file_path = Storage::disk('public')->put('upload/cv', $data['CV']);
+    //             $data["CV"] = $file_path;
+    //         }
 
-//          $chef->update($data);
+    //          $chef->update($data);
 
-// //       Parentesi relazione. Senza parentesi chiamo il model
-//          $chef->specializations()->sync($data['specializations']);
+    // //       Parentesi relazione. Senza parentesi chiamo il model
+    //          $chef->specializations()->sync($data['specializations']);
 
     //     $chef->loadMissing('specializations');
     //     return response()->json(
@@ -112,48 +113,40 @@ class ChefController extends Controller
         $vote = $request->input('vote');
         $reviews = $request->input('reviews');
 
+        // Start building the query
         $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')
-            ->withCount('reviews');
+            ->withCount('reviews')
+            // Join the sponsorships table to order by sponsorships.id
+            ->leftJoin('chef_sponsorship', 'chefs.id', '=', 'chef_sponsorship.chef_id')
+            ->leftJoin('sponsorships', 'chef_sponsorship.sponsorship_id', '=', 'sponsorships.id')
+            ->orderBy('sponsorships.id', 'desc');
 
-        // Aggiungi una sottoquery per calcolare la media dei voti
+        // Add a subquery to calculate the average vote
         $chefs = $chefs->addSelect([
             'average_vote' => Vote::select(DB::raw('AVG(votes.vote)'))
                 ->join('chef_vote', 'votes.id', '=', 'chef_vote.vote_id')
                 ->whereColumn('chef_vote.chef_id', 'chefs.id')
         ]);
 
-        // Applica il filtro per le specializzazioni se presente
+        // Apply filter for specializations if present
         if (!empty($specializationIds)) {
             $chefs = $chefs->whereHas('specializations', function ($query) use ($specializationIds) {
                 $query->whereIn('specializations.id', $specializationIds);
             });
         }
 
-        // Applica il filtro per il voto medio se presente
+        // Apply filter for average vote if present
         if (!empty($vote)) {
             $chefs = $chefs->having('average_vote', '>=', $vote);
         }
 
-        // Applica il filtro per il numero di recensioni se presente
+        // Apply filter for the number of reviews if present
         if (!empty($reviews)) {
             $chefs = $chefs->having('reviews_count', '>=', $reviews);
         }
 
-        // Esegui la query
+        // Execute the query
         $chefs = $chefs->get();
-
-        // if (!empty($specializationIds)) {
-        //     // Filter chefs by the given specialization IDs
-        //     $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')
-        //         ->whereHas('specializations', function ($query) use ($specializationIds) {
-        //             $query->whereIn('specializations.id', $specializationIds);
-        //         })
-        //         ->get();
-        // } else {
-        //     // If no IDs provided, return all chefs (or you can choose to return an empty response)
-        //     $chefs = Chef::with('user', 'sponsorships', 'specializations', 'votes', 'reviews')
-        //         ->get();
-        // }
 
         // Return the results in the JSON response
         return response()->json([
@@ -162,7 +155,9 @@ class ChefController extends Controller
         ]);
     }
 
-    public function searchSpecializationHome(Request $request){
+
+    public function searchSpecializationHome(Request $request)
+    {
         $specializationIds = $request->input('id');
     }
 }
