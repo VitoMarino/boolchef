8852118f04ace6503b2b8eb74a5925685335ad7f
MatteoDiMario96 <dm.matteo96@live.it>
2024-09-18 23:40:49 +0200

    Aggiunta una piccola sezione per vedere le tue sponsorizzazioni attive se ne hai
    Aggiunto il form che serve per il pagamento checkout

--- resources/views/admin/chefs/profile/sponsorship.blade.php
+++ resources/views/admin/chefs/profile/sponsorship.blade.php
@@ -2,12 +2,44 @@
 
 @section('content')
     <h1 class="text-center">
-        Sponsorizzazioni
+        Scegli un pacchetto di Sponsorizzazioni
         <i class="fa-solid fa-sack-dollar"></i>
     </h1>
     <hr class="mb-5">
 
+    @if(session('not-auth'))
+        <div class="alert alert-danger">
+            {{session('not-auth')}}
+        </div>
+    @endif
+
     <div class="d-flex justify-content-center">
+        @if ($chef->isSponsored())
+            <div>
+                <h4>La tua sponsorizzazione attuale:</h4>
+
+                @foreach ($chef->sponsorships as $sponsorship)
+                    @if ($sponsorship->pivot->start_date <= now() && $sponsorship->pivot->end_date >= now())
+                        <h5>Tipo: {{$sponsorship->name}}</h5>
+                        <p>Inizio: {{ $sponsorship->pivot->start_date }}</p>
+                        <p>Fine: {{ $sponsorship->pivot->end_date }}</p>
+                    @endif
+                @endforeach
+            </div>
+
+        @else
+
+        <div>
+            <h4>Non hai sponsorizzazioni attive</h4>
+            <p>Acquistane una per i vantaggi eslusivi!</p>
+        </div>
+
+        @endif
+
+
+
+
+
         <div class="card me-3" style="width: 18rem;">
             <div class="card-body">
                 <h4>
@@ -18,6 +50,13 @@
                     2,99 € per 24 ore di sponsorizzazione
                 </p>
             </div>
+            <form action="{{ route('admin.sponsorships.checkout') }}" method="POST" id="payment-form-1">
+                @method("POST")
+                @csrf
+                <input type="hidden" name="sponsorship_id" value="1">
+                <div id="dropin-container-1"></div>
+                <button type="submit" class="btn btn-primary">Acquista</button>
+            </form>
         </div>
 
         <div class="card me-3" style="width: 18rem;">
@@ -31,6 +70,13 @@
                     5,99 € per 72 ore di sponsorizzazione
                 </p>
             </div>
+            <form action="{{ route('admin.sponsorships.checkout') }}" method="POST" id="payment-form-2">
+                @method("POST")
+                @csrf
+                <input type="hidden" name="sponsorship_id" value="2">
+                <div id="dropin-container-2"></div>
+                <button type="submit" class="btn btn-primary">Acquista</button>
+            </form>
         </div>
 
         <div class="card" style="width: 18rem;">
@@ -45,7 +91,23 @@
                     9,99 € per 144 ore di sponsorizzazione
                 </p>
             </div>
+            <form action="{{ route('admin.sponsorships.checkout') }}" method="POST" id="payment-form-3">
+                @method("POST")
+                @csrf
+                <input type="hidden" name="sponsorship_id" value="3">
+                <div id="dropin-container-3"></div>
+                <button type="submit" class="btn btn-primary">Acquista</button>
+            </form>
         </div>
     </div>
 
+@endsection
+
+
+@section('scripts')
+<script src="https://js.braintreegateway.com/web/dropin/1.31.0/js/dropin.min.js"></script>
+<script>
+    window.clientToken = @json($clientToken);
+</script>
+@vite('resources/js/myScript/sponsorshipBraintree.js')
 @endsection
